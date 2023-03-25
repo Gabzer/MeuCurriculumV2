72ee93c323e08625b7a4684c39ac00661bc4eecf
Gabriel Zerbine <gab.zer.nas@gmail.com>
2023-03-25 15:50:30 -0400

    first files and i18next

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useTranslation, Trans } from 'react-i18next';
+
+import logo from './logo.svg';
+import './App.css';
+
+const lngs: {[index: string]:any} = {
+  en: { nativeName: 'English' },
+  de: { nativeName: 'Deutsch' }
+};
+
+function App() {
+  const { t, i18n } = useTranslation();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          {Object.keys(lngs).map((lng: string) => (
+            <button key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit" onClick={() => i18n.changeLanguage(lng)}>
+              {lngs[lng].nativeName}
+            </button>
+          ))}
+        </div>
+        <p>
+          <Trans i18nKey="description.part1">
+            Edit <code>src/App.js</code> and save to reload.
+          </Trans>
+        </p>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {t('description.part2')}
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
